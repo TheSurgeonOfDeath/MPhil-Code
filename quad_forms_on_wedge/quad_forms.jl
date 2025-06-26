f4b0ff5cd57c00bbdaf6a699742aaa131c5d2336
TheSurgeonOfDeath <83719688+TheSurgeonOfDeath@users.noreply.github.com>
2025-06-26 14:28:23 +0930

    put things in functions

--- quad_forms_on_wedge/quad_forms.jl
+++ quad_forms_on_wedge/quad_forms.jl
@@ -3,12 +3,57 @@ using LinearAlgebra
 using SparseArrays
 using Printf
 
-# Define parameters
-const n = 6
-const wedge_basis_idx = collect(combinations(1:n, 2))
-const quad_forms_basis_idx = collect(combinations(1:n, 4))
-const k = length(wedge_basis_idx)
-const m = length(quad_forms_basis_idx)
+
+# Function to compute unique signatures of quadrilinear forms
+function unique_signatures_quad_forms(n::Int)
+    wedge_basis_idx = collect(combinations(1:n, 2))
+    quad_forms_basis_idx = collect(combinations(1:n, 4))
+    # k = length(wedge_basis_idx)
+    m = length(quad_forms_basis_idx)
+
+    # Precompute symmetric matrices
+    mats = [symm_matrix_quad_form(wedge_basis_idx, q) for q in quad_forms_basis_idx]
+
+    # Signature cache
+    seen_signatures = Dict{Tuple{Int, Int, Int}, Bool}()
+
+    # Find unique signatures
+    unique_signatures = Vector{Tuple{Int, Int, Int}}()
+    unique_signatures_quad_forms = Vector{Vector{Vector{Int}}}()
+    for i in 1:n
+        for combo in combinations(1:m, i)
+
+            # Sum the matrices for the current combination
+            sum_mat = spzeros(Int, size(mats[1])...)
+            for idx in combo
+                sum_mat .+= mats[idx]
+            end
+
+            sgn = signature_matrix(sum_mat)
+            if !haskey(seen_signatures, sgn)
+                seen_signatures[sgn] = true
+                push!(unique_signatures, sgn)
+                push!(unique_signatures_quad_forms, quad_forms_basis_idx[combo])
+            end
+        end
+    end
+
+    return (unique_signatures, unique_signatures_quad_forms)
+end
+
+function print_signatures(signatures::Vector{Tuple{Int, Int, Int}}, quad_forms_basis_idx::Vector{Vector{Vector{Int}}})
+     # Output file
+    filename = "data Julia/unique_sgns_$(n)_julia.csv"
+    open(filename, "w") do io
+        for (sgn,combo) in zip(signatures, quad_forms_basis_idx)
+            @printf(io, "%d,%d,%d", sgn...)
+            for q in combo
+                @printf(io, ",[%s]", join(q, " "))
+            end
+            write(io, "\n")
+        end
+    end
+end
 
 # Construct symmetric matrices
 
@@ -38,34 +83,6 @@ function signature_matrix(mat::SparseMatrixCSC{Int, Int})
     return (p, q, r)
 end
 
-
-# Precompute symmetric matrices
-mats = [symm_matrix_quad_form(wedge_basis_idx, q) for q in quad_forms_basis_idx]
-
-# Signature cache
-seen_signatures = Dict{Tuple{Int, Int, Int}, Bool}()
-
-# Output file
-filename = "data Julia/unique_sgns_$(n)_julia.csv"
-open(filename, "w") do io
-    for i in 1:n
-        for combo in combinations(1:m, i)
-
-            # Sum the matrices for the current combination
-            sum_mat = spzeros(Int, size(mats[1])...)
-            for idx in combo
-                sum_mat .+= mats[idx]
-            end
-
-            sgn = signature_matrix(sum_mat)
-            if !haskey(seen_signatures, sgn)
-                seen_signatures[sgn] = true
-                @printf(io, "%d,%d,%d", sgn...)
-                for idx in combo
-                    @printf(io, ",[%s]", join(quad_forms_basis_idx[idx], " "))
-                end
-                write(io, "\n")
-            end
-        end
-    end
-end
+n = 6
+unique_signatures_forms = unique_signatures_quad_forms(n)
+print_signatures(unique_signatures_forms[1], unique_signatures_forms[2])
