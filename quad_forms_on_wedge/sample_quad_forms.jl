0e2cc0c05bebb21f66ec8f8d57d1d22c238f468f
TheSurgeonOfDeath <83719688+TheSurgeonOfDeath@users.noreply.github.com>
2025-07-03 12:58:15 +0930

    benchmarked old way and new way of generating random symmetric matrices for quad forms

--- quad_forms_on_wedge/sample_quad_forms.jl
+++ quad_forms_on_wedge/sample_quad_forms.jl
@@ -1,14 +1,8 @@
 using LinearAlgebra
 using Combinatorics
 using StatsBase
+using BenchmarkTools
 
-# Define basis of Λ²(ℝ⁷)
-n = 7
-Λ2_basis = collect(combinations(1:n, 2))  # index pairs (i,j), i < j
-Λ4_basis = collect(combinations(1:n, 4))  # index tuples (i,j,k,l), i<j<k<l
-
-# Map: (i,j) ↦ basis index in Λ²(ℝ⁷)
-Λ2_index = Dict((i,j) => idx for (idx, (i,j)) in enumerate(Λ2_basis))
 
 # Compute sign of permutation needed to bring (i,j,k,l) to sorted order
 function wedge_sign(i, j, k, l)
@@ -62,6 +56,21 @@ function random_4form()
     return q
 end
 
+
+function symm_matrix_quad_form(wedge_basis_idx::Vector{Vector{Int}}, q::Vector{Int})
+    elem_idx = collect(combinations(1:4, 2))
+    k = length(wedge_basis_idx)
+    mat = zeros(Int, k, k)
+    vals = [1, -1, 1]
+    for i in 1:3
+        row = findfirst(x -> x == q[elem_idx[i]], wedge_basis_idx)
+        col = findfirst(x -> x == q[elem_idx[7 - i]], wedge_basis_idx)
+        mat[row, col] = vals[i]
+        mat[col, row] = vals[i]
+    end
+    return mat
+end
+
 # Sample many 4-forms and collect observed signatures
 function collect_signatures(num_trials::Int)
     signatures = []
@@ -74,12 +83,47 @@ function collect_signatures(num_trials::Int)
     return countmap(signatures)
 end
 
+function random_qform_matrix()
+    coeffs = randn(length(Λ4_basis_matrices))
+    q = zeros(size(Λ4_basis_matrices[1]))
+    for i in eachindex(Λ4_basis_matrices)
+        q += coeffs[i] * Λ4_basis_matrices[i]
+    end
+    return q
+end
+
+function collect_signatures2(num_trials::Int)
+    signatures = []
+    for _ in 1:num_trials
+        Bq = random_qform_matrix()
+        sig = signature(Bq)
+        push!(signatures, sig)
+    end
+    return countmap(signatures)
+end
+
+
+# Define basis of Λ²(ℝ⁷)
+n = 7
+Λ2_basis = collect(combinations(1:n, 2))  # index pairs (i,j), i < j
+Λ4_basis = collect(combinations(1:n, 4))  # index tuples (i,j,k,l), i<j<k<l
+
+# Map: (i,j) ↦ basis index in Λ²(ℝ⁷)
+Λ2_index = Dict((i,j) => idx for (idx, (i,j)) in enumerate(Λ2_basis))
+
+Λ4_basis_matrices = [symm_matrix_quad_form(Λ2_basis, q) for q in Λ4_basis]
+
+
 
 # q = random_4form()
 # Bq = build_Bq(q)
 # sig = signature(Bq)
 # push!(signatures, sig)
 # sig_counts = countmap(signatures)
+
+@benchmark collect_signatures(1_000)
+@benchmark collect_signatures2(1_000)
+
 
 # Example: run 1000 trials
 sig_counts = collect_signatures(1_000_000)
